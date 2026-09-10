9184e7b1397c348f123672b2e27ef2a0b7195c0d
aakash1803 <saakash1803@gmail.com>
2026-09-11 00:54:37 +0530

    chore: initialize Spring Boot backend

---
+++ backend/src/main/java/com/salarymanagement/SalaryManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.salarymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalaryManagementBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalaryManagementBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/salarymanagement/SalaryManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salarymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalaryManagementBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
